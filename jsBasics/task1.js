c31d04ddcda3f31daec87e90c1ec29db363f3979
Vladyslav-Riabets <ryabets.vlad@gmail.com>
2025-11-12 15:36:49 +0200

    Load homework files files for JS

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+let testString = "Test value";
+let testNum = 100;
+let testBool = true;
+let testNull = null;
+let testUndefined;
+
+console.log(testString);
+console.log(testNum);
+console.log(testBool);
+console.log(testNull);
+console.log(testUndefined);
