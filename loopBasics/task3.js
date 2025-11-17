a46c7cc4fca740248b50f8d378107a20db46ee6b
Vladyslav-Riabets <ryabets.vlad@gmail.com>
2025-11-17 11:39:12 +0200

    Updated task3.js file

--- loopBasics/task3.js
+++ loopBasics/task3.js
@@ -1,11 +1,11 @@
 const number = 5;
 
-console.log (`=== Таблиця множення для ${number} через for ===`)
+console.log (`=== Таблиця множення для ${number} через for ===`);
 for (let i = 1; i <= 10; i++) {
     console.log(`${number} * ${i} = ${number * i}`);
 }
 
-console.log (`=== Таблиця множення для ${number} через while ===`)
+console.log (`=== Таблиця множення для ${number} через while ===`);
 let n = 1;
 while (n <= 10) {
     console.log(`${number} * ${n} = ${number * n}`);
